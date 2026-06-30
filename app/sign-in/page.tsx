57485684b351f12eeab15b1a0008509e17ca2f99
John Rafael <johnrafaelquispeccorimanya@gmail.com>
2026-06-29 20:16:33 -0500

    Remove auth pages

--- app/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs"
-
-export default function SignInPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#7C4DFF]/10 to-[#4D9FFF]/10">
-      <SignIn />
-    </div>
-  )
-}
--- app/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs"
-
-export default function SignUpPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#7C4DFF]/10 to-[#4D9FFF]/10">
-      <SignUp />
-    </div>
-  )
-}
